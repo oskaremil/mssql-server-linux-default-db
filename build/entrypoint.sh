ce45beb4ae9d945045ad8cfe57099328aee7d2da
Oskar Emil Skeide <oskaremil@gmail.com>
2018-03-08 23:00:26 +0100

    add tail command to keep container open

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,2 +1,3 @@
 #start SQL Server, start the script to create the DB
-/opt/mssql/bin/sqlservr & /usr/src/default-db/import-data.sh
+
+/opt/mssql/bin/sqlservr & /usr/src/default-db/import-data.sh && tail -f /etc/hosts
